06926a8bc10642bc23f8a63fcdbc0dfc732b3e5f
angga <angga.prabowo@dataon.com>
2025-07-28 00:21:58 +0700

    add readme.md

--- apps/serial/preload/index.js
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-contextBridge.exposeInMainWorld('api', {
-    platform: async () => {
-        return await ipcRenderer.invoke('get-platform');
-    }
-});
